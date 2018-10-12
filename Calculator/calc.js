0ed372e432b8282f77dd312a6d24c198ced92689
aks97cs <aks97cs@gmail.com>
2018-10-12 15:21:38 +0530

    Modify

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -4,6 +4,7 @@
             var res;
             var exp;
             var explen;
+            var aftropexp = '0';
             //console.log(op);
             document.getElementById("demo").value="hello world";
             var str = document.getElementById("demo").value;
@@ -33,26 +34,314 @@
                     }
                     else
                     {
-                        num2 = 1;
+                        aftropexp = aftropexp + '1';
+                        document.getElementById('demo').value = exp+op+aftropexp;
+                        num2 = parseInt(aftropexp,10);
+                        console.log("afterexp = "+ aftropexp);
+                        console.log("num2 ="+num2 );
+                        
                     }
                     
                   
-                    result();
+                   // result();
                 }
             }
             function two()
             {
+                console.log(op);
                 var ch = document.getElementById("demo").value;
+                console.log(ch);
                 if(ch=='hello word')
                 {
-                    document.getElementById('demo').value="2";
-                    num1=2;
+                    
+                    exp = '2';
+                    document.getElementById('demo').value=exp[0];
+                    num1 =  parseInt(exp,10);
                 }
                 else
                 {
-                    document.getElementById('demo').value="2";
-                    num2 = 2;
-                    result();
+                    console.log("boom");
+                    if(op == undefined)
+                    {
+                        exp = exp + '2';
+                        document.getElementById('demo').value=exp;
+                        num1 = parseInt(exp,10);
+                        console.log("exp = " + exp);
+                        console.log("num1 = "+num1);
+                    }
+                    else
+                    {
+                        aftropexp = aftropexp + '2';
+                        document.getElementById('demo').value = exp+op+aftropexp;
+                        num2 = parseInt(aftropexp,10);
+                        console.log("afterexp = "+ aftropexp);
+                        console.log("num2 ="+num2 );
+                        
+                    }
+                }
+              
+            }
+            function three()
+            {
+                var ch = document.getElementById("demo").value;
+                if(ch=='hello word')
+                {
+                    
+                    exp = '3';
+                    document.getElementById('demo').value=exp[0];
+                    num1 =  parseInt(exp,10);
+                }
+                else
+                {
+
+                    if(op == undefined)
+                    {
+                        exp = exp + '3';
+                        document.getElementById('demo').value=exp;
+                        num1 = parseInt(exp,10);
+                        console.log("exp = " + exp);
+                        console.log("num1 = "+num1);
+                    }
+                    else
+                    {
+                        aftropexp = aftropexp + '3';
+                        document.getElementById('demo').value = exp+op+aftropexp;
+                        num2 = parseInt(aftropexp,10);
+                        console.log("afterexp = "+ aftropexp);
+                        console.log("num2 ="+num2 );
+                        
+                    }
+                }
+              
+            }
+            function four()
+            {
+                var ch = document.getElementById("demo").value;
+                if(ch=='hello word')
+                {
+                    
+                    exp = '4';
+                    document.getElementById('demo').value=exp[0];
+                    num1 =  parseInt(exp,10);
+                }
+                else
+                {
+
+                    if(op == undefined)
+                    {
+                        exp = exp + '4';
+                        document.getElementById('demo').value=exp;
+                        num1 = parseInt(exp,10);
+                        console.log("exp = " + exp);
+                        console.log("num1 = "+num1);
+                    }
+                    else
+                    {
+                        aftropexp = aftropexp + '4';
+                        document.getElementById('demo').value = exp+op+aftropexp;
+                        num2 = parseInt(aftropexp,10);
+                        console.log("afterexp = "+ aftropexp);
+                        console.log("num2 ="+num2 );
+                        
+                    }
+                }
+              
+            }
+            function five()
+            {
+                var ch = document.getElementById("demo").value;
+                if(ch=='hello word')
+                {
+                    
+                    exp = '5';
+                    document.getElementById('demo').value=exp[0];
+                    num1 =  parseInt(exp,10);
+                }
+                else
+                {
+
+                    if(op == undefined)
+                    {
+                        exp = exp + '5';
+                        document.getElementById('demo').value=exp;
+                        num1 = parseInt(exp,10);
+                        console.log("exp = " + exp);
+                        console.log("num1 = "+num1);
+                    }
+                    else
+                    {
+                        aftropexp = aftropexp + '5';
+                        document.getElementById('demo').value = exp+op+aftropexp;
+                        num2 = parseInt(aftropexp,10);
+                        console.log("afterexp = "+ aftropexp);
+                        console.log("num2 ="+num2 );
+                        
+                    }
+                }
+              
+            }
+            function six()
+            {
+                var ch = document.getElementById("demo").value;
+                if(ch=='hello word')
+                {
+                    
+                    exp = '6';
+                    document.getElementById('demo').value=exp[0];
+                    num1 =  parseInt(exp,10);
+                }
+                else
+                {
+
+                    if(op == undefined)
+                    {
+                        exp = exp + '6';
+                        document.getElementById('demo').value=exp;
+                        num1 = parseInt(exp,10);
+                        console.log("exp = " + exp);
+                        console.log("num1 = "+num1);
+                    }
+                    else
+                    {
+                        aftropexp = aftropexp + '6';
+                        document.getElementById('demo').value = exp+op+aftropexp;
+                        num2 = parseInt(aftropexp,10);
+                        console.log("afterexp = "+ aftropexp);
+                        console.log("num2 ="+num2 );
+                        
+                    }
+                }
+              
+            }
+            function seven()
+            {
+                var ch = document.getElementById("demo").value;
+                if(ch=='hello word')
+                {
+                    
+                    exp = '7';
+                    document.getElementById('demo').value=exp[0];
+                    num1 =  parseInt(exp,10);
+                }
+                else
+                {
+
+                    if(op == undefined)
+                    {
+                        exp = exp + '7';
+                        document.getElementById('demo').value=exp;
+                        num1 = parseInt(exp,10);
+                        console.log("exp = " + exp);
+                        console.log("num1 = "+num1);
+                    }
+                    else
+                    {
+                        aftropexp = aftropexp + '7';
+                        document.getElementById('demo').value = exp+op+aftropexp;
+                        num2 = parseInt(aftropexp,10);
+                        console.log("afterexp = "+ aftropexp);
+                        console.log("num2 ="+num2 );
+                        
+                    }
+                }
+              
+            }
+            function eight()
+            {
+                var ch = document.getElementById("demo").value;
+                if(ch=='hello word')
+                {
+                    
+                    exp = '8';
+                    document.getElementById('demo').value=exp[0];
+                    num1 =  parseInt(exp,10);
+                }
+                else
+                {
+
+                    if(op == undefined)
+                    {
+                        exp = exp + '8';
+                        document.getElementById('demo').value=exp;
+                        num1 = parseInt(exp,10);
+                        console.log("exp = " + exp);
+                        console.log("num1 = "+num1);
+                    }
+                    else
+                    {
+                        aftropexp = aftropexp + '8';
+                        document.getElementById('demo').value = exp+op+aftropexp;
+                        num2 = parseInt(aftropexp,10);
+                        console.log("afterexp = "+ aftropexp);
+                        console.log("num2 ="+num2 );
+                        
+                    }
+                }
+              
+            }
+            function nine()
+            {
+                var ch = document.getElementById("demo").value;
+                if(ch=='hello word')
+                {
+                    
+                    exp = '9';
+                    document.getElementById('demo').value=exp[0];
+                    num1 =  parseInt(exp,10);
+                }
+                else
+                {
+
+                    if(op == undefined)
+                    {
+                        exp = exp + '9';
+                        document.getElementById('demo').value=exp;
+                        num1 = parseInt(exp,10);
+                        console.log("exp = " + exp);
+                        console.log("num1 = "+num1);
+                    }
+                    else
+                    {
+                        aftropexp = aftropexp + '9';
+                        document.getElementById('demo').value = exp+op+aftropexp;
+                        num2 = parseInt(aftropexp,10);
+                        console.log("afterexp = "+ aftropexp);
+                        console.log("num2 ="+num2 );
+                        
+                    }
+                }
+              
+            }
+            function zero()
+            {
+                var ch = document.getElementById("demo").value;
+                if(ch=='hello word')
+                {
+                    
+                    exp = '0';
+                    document.getElementById('demo').value=exp;
+                    num1 =  parseInt(exp,10);
+                }
+                else
+                {
+
+                    if(op == undefined)
+                    {
+                        exp = exp + '0';
+                        document.getElementById('demo').value=exp;
+                        num1 = parseInt(exp,10);
+                        console.log("exp = " + exp);
+                        console.log("num1 = "+num1);
+                    }
+                    else
+                    {
+                        aftropexp = aftropexp + '0';
+                        document.getElementById('demo').value = exp+op+aftropexp;
+                        num2 = parseInt(aftropexp,10);
+                        console.log("afterexp = "+ aftropexp);
+                        console.log("num2 ="+num2 );
+                        
+                    }
                 }
               
             }
@@ -67,5 +356,10 @@
                 {
                     document.getElementById('demo').value = num1+num2;
                 }
+            }
+            function equal()
+            {
+                var rs = num1+num2;
+                document.getElementById('demo').value = exp+op+aftropexp+'='+rs;
             }
            
