7c6aee77c281a62189e0a6e6239f0ee41ff27862
aks97cs <aks97cs@gmail.com>
2018-10-13 12:27:13 +0530

    node initiate

---
+++ Inventory/js/min.main.js
@@ -0,0 +1,9 @@
+function inventory()
+{
+    var a = "<h3> hello love</h3>";
+    console.log("test");
+    document.getElementById('main').innerHTML = "<h3>hello</h3>" + a;
+    //document.getElementsByClassName('cbody').innerHTML="dfdsf";
+    //alert("test");
+    console.log("test");
+}
